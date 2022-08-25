7a2fa179da28ddfa649ef67048f0b5a92695f1ca
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-08-25 15:09:41 +0700

    signin form responsive

--- frontend/src/containers/Verify/Verify.jsx
+++ frontend/src/containers/Verify/Verify.jsx
@@ -5,52 +5,45 @@ import SignUpForm from "../../components/Verify/SignUpForm";
 import styles from "./Verify.module.css";
 
 const Verify = () => {
-    const [modalShow, setModalShow] = useState(true);
+    const [modalShow, setModalShow] = useState(false);
     return (
         <div id={styles["Verify"]}>
-            <Container>
-                <Row className={styles["Verify-Container"]}>
-                    <Col lg={6}>
-                        <h1 className={styles["Verify-Title"]}>SpaceCapt Social Media</h1>
-                        <p>helps you connect and share with the people in your life.</p>
-                    </Col>
-                    <Col
-                        lg={6}
-                        style={{
-                            display: "flex",
-                            justifyContent: "center",
-                        }}
-                    >
-                        <div className={styles["Form-SignIn-Container-Parent"]}>
-                            <form action="" className={styles["Form-SignIn-Container"]}>
-                                <div
-                                    className={styles["Form-SignIn-Input-Wrapper"]}
-                                    style={{
-                                        marginTop: "unset",
-                                    }}
-                                >
-                                    <input type="text" required />
-                                    <label htmlFor="">Username</label>
-                                </div>
-                                <div className={styles["Form-SignIn-Input-Wrapper"]}>
-                                    <input type="password" required />
-                                    <label htmlFor="">Password</label>
-                                </div>
-                                <button type="submit" className={styles["Form-SignIn-Input-Button-Submit"]}>
-                                    Log In
+            <Row className={styles["Verify-Container"]}>
+                <Col md={6}>
+                    <h1 className={styles["Verify-Title"]}>SpaceCapt Social Media</h1>
+                    <p className={styles["Verify-Title2"]}>helps you connect and share with the people in your life.</p>{" "}
+                </Col>
+                <Col md={6}>
+                    <div className={styles["Form-SignIn-Container-Parent"]}>
+                        <form action="" className={styles["Form-SignIn-Container"]}>
+                            <div
+                                className={styles["Form-SignIn-Input-Wrapper"]}
+                                style={{
+                                    marginTop: "unset",
+                                }}
+                            >
+                                <input type="text" required />
+                                <label htmlFor="">Username</label>
+                            </div>
+                            <div className={styles["Form-SignIn-Input-Wrapper"]}>
+                                <input type="password" required />
+                                <label htmlFor="">Password</label>
+                            </div>
+                            <button type="submit" className={styles["Form-SignIn-Input-Button-Submit"]}>
+                                Log In
+                            </button>
+                            <p className={styles["Form-SignIn-Forgoten-Password"]}>Forgotten password?</p>
+                            <div className={styles["Form-SignIn-Border"]}></div>
+                            <div className={styles["Form-SignIn-Input-Button-Register-Container"]}>
+                                <button onClick={() => setModalShow(true)} className={styles["Form-SignIn-Input-Button-Register"]}>
+                                    Create New Account
                                 </button>
-                                <p className={styles["Form-SignIn-Forgoten-Password"]}>Forgotten password?</p>
-                                <div className={styles["Form-SignIn-Border"]}></div>
-                                <div className={styles["Form-SignIn-Input-Button-Register-Container"]}>
-                                    <button onClick={() => setModalShow(true)} className={styles["Form-SignIn-Input-Button-Register"]}>
-                                        Create New Account
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
+                            </div>
+                        </form>
+                    </div>{" "}
+                </Col>
+            </Row>
+
             <SignUpForm show={modalShow} onHide={() => setModalShow(false)} />
         </div>
     );
